e2229e08a6b45ab978da5e5705f03c50eb9b0723
haoyangkong99 <haoyangkong@gmail.com>
2021-11-20 21:17:21 +0800

    new_branch_kong

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:jomshare/screens/user%20authentication/createProfile.dart';
 import 'package:jomshare/screens/user%20authentication/forgetpassword.dart';
+import 'package:jomshare/screens/user%20authentication/license.dart';
 import 'package:jomshare/screens/welcome/welcome_screen.dart';
 import 'package:jomshare/screens/splash/splash.dart';
 import 'package:jomshare/screens/home/home.dart';
 import 'package:jomshare/screens/user authentication/login.dart';
 import 'package:jomshare/screens/user authentication/register.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 
 
 
-void main() {
+Future<void> main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
@@ -33,6 +37,7 @@ routes: {
   '/home': (context)=>Home(),
   '/forgetpass': (context)=>forgetpassword(),
   '/register/createProfile': (context)=>createProfile(),
+  '/drivingProfile': (context)=>license(),
 
 });
   }
